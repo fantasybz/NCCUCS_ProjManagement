26ba2f5a712867e12182a2bfa8720633bdb70f03
fantasybz <fantasybz@gmail.com>
2015-07-16 04:02:57 +0800

    新增updateTenantsPageFileContentRemote  for AJAX Remote Call

--- App_Code/webservice/TenantsPageWebService.cs
+++ App_Code/webservice/TenantsPageWebService.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Script.Serialization;
+using System.Web.Script.Services;
 using System.Web.Services;
 
 /// <summary>
@@ -53,5 +55,37 @@ public class TenantsPageWebService : System.Web.Services.WebService
 
         return JSONSerializer.Serialize(wsro);
     }
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+    public void updateTenantsPageFileContentRemote(int pageId, string fileContent, string layoutitContent, string callback)
+    {
+        string appPath = HttpContext.Current.Request.ApplicationPath;
+        string physicalPath = HttpContext.Current.Request.MapPath(appPath);
+
+        string MTAPageRoot = physicalPath + Resources.Resource.MTAUIPageDir;
+
+        WSResultObj wsro = tps.updateTenantsPageFileContent(MTAPageRoot, pageId, fileContent, layoutitContent);
+
+
+        // Method 1: use built-in serializer:
+        StringBuilder sb = new StringBuilder();
+        sb.Append(callback + "(");
+        sb.Append(JSONSerializer.Serialize(wsro));
+        sb.Append(");");
+
+        // Method 2: NewtonSoft JSON serializer (delete as applicable)
+        // StringBuilder sb = new StringBuilder();
+        // sb.Append(callback + "(");
+        // sb.Append(JsonConvert.SerializeObject(videos, Formatting.Indented)); // indentation is just for ease of reading while testing
+        // sb.Append(");");     
+
+        Context.Response.Clear();
+        Context.Response.ContentType = "application/json";
+        Context.Response.Write(sb.ToString());
+        Context.Response.End();
+    }
+
+
 
 }
